b8857000fe04b6a0ac4607b1acaeb1a01a2b1c7a
sumedhars <SaiRam@6454>
2023-04-05 15:33:06 -0500

    nnn

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -1,25 +1,47 @@
 import pandas as pd
 import patsy
 import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 xls = pd.ExcelFile('API_ProjectAll_SI.xlsx')
 staticDenDataframe = pd.read_excel(xls, 'staticDen')
 staticViscDataframe = pd.read_excel(xls, 'staticVisc')
 
-selectStaticDen = staticDenDataframe[['T', 'ALogP', 'ALogP2', 'AMR']].copy()
-selectStaticVisc = staticViscDataframe[['T', 'ALogP', 'ALogP2', 'AMR']].copy()
+selectStaticDen = staticDenDataframe[['Density', 'T', 'ALogP', 'ALogP2', 'AMR']].copy()
+selectStaticVisc = staticViscDataframe[['cP', 'T', 'ALogP', 'ALogP2', 'AMR']].copy()
 
-print("Static Density Data Analysis")
-yT, X = patsy.dmatrices("T ~ ALogP + ALogP2 + AMR", data=selectStaticDen, return_type="dataframe")
-X.info()
-model = sm.OLS(yT, X)
-results = model.fit()
-print(results.summary())
+# grouping relevant data together
+finalDataDF = selectStaticVisc.copy()
+density = selectStaticDen['Density'].to_list()
+finalDataDF['Density'] = density
 
-print()
-print(print("Static Viscosity Data Analysis"))
-yT, X = patsy.dmatrices("T ~ ALogP + ALogP2 + AMR", data=selectStaticVisc, return_type="dataframe")
-X.info()
-model = sm.OLS(yT, X)
-results = model.fit()
-print(results.summary())
+# Helper functions
+def norm(x):
+    return (x - train_stats['mean']) / train_stats['std']
+def format_output(data):
+    y1 = data.pop('PRICE')
+    y1 = np.array(y1)
+    y2 = data.pop('PTRATIO')
+    y2 = np.array(y2)
+    return y1, y2
+
+# splitting data
+train, test = train_test_split(selectStaticDen, test_size=0.2, random_state=1)
+train, val = train_test_split(train, test_size=0.2, random_state=1)
+
+train_stats = train.describe()
+train_stats.pop('Density')
+train_stats.pop('cP')
+train_stats = train_stats.transpose()
+train_Y = format_output(train)
+test_Y = format_output(test)
+val_Y = format_output(val)
+
+# Normalize the training and test data
+norm_train_X = np.array(norm(train))
+norm_test_X = np.array(norm(test))
+norm_val_X = np.array(norm(val))
